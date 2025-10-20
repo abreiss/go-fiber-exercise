9ba349d5ec9cc9b707f4072d56de7252757a7fcf
abreiss <nicoreiss123@gmail.com>
2025-10-19 19:01:50 -0700

    testing for demo

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
-//this app adds a JSON response with name and a dynamic timestamp
+//this app adds a JSON response with your name and a dynamic timestamp
 // and ensures the JSON output is minified
 package main
 
 import (
-	"github.com/gofiber/fiber/v2" 	// go framework
-	"time" 							//used for timestamp
-	"os"							//read port and env variables
-	"fmt"							//used for printing
-	"encoding/json" 				//used for json
+"github.com/gofiber/fiber/v2" 	// go framework
+"time" 							//used for timestamp
+"os"							//read port and env variables
+"fmt"							//used for printing
+"encoding/json" 				//used for json
 )
 //structure of json response
 // {“message”: “My name is ___”, “timestamp”: 123456789}
@@ -25,6 +25,7 @@ func main() {
 			"timestamp": time.Now().UnixMilli(),
 			//add deploy time to adjust json
 			//"deployed_at": time.Now().Format("2006-01-02"),
+			"deployed_at": time.Now().Format("2006-01-02"),
 		}
 		//set type to json
 		c.Set("Content-Type", "application/json")
@@ -36,14 +37,13 @@ func main() {
 	//if port is not specified set it to 80
 	port := os.Getenv("PORT")
 	if port == "" {
-   		port = "80" // local dev (you can use "3000" if you prefer)
+		port = "80" // local dev
 	}
-
 	//list to all  ip address at port
 	addr := "0.0.0.0:" + port
 	//print where the server is hosted 
 	fmt.Println("Server on http://" + "localhost:" + port)
-	if err := app.(addr); err != nil {
-		panic(err)
+	if err := app.Listen(addr); err != nil {
+	panic(err)
 	}
 }
